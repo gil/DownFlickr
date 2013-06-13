69059bed9e4fbbb555d4c699d3d1f08f22b2b26d
André Gil <contato@andregil.org>
2013-06-13 16:38:23 -0300

    updating jquery and refactoring some details

--- background.js
+++ background.js
@@ -1,21 +1,30 @@
-chrome.extension.onRequest.addListener( function(request, sender, sendResponse) {
+(function() {
 
-		/**
-		 * Used to avoid problems when opening many photos
-		 */
-		if (request.msg == "openAllPhotos")
-		{
+	"use strict";
 
-			if( confirm("Are you sure you want to open " + request.imgs.length + " tab(s)? It may take a while to download everything.") )
+	chrome.extension.onRequest.addListener( function(request, sender, sendResponse) {
+
+			/**
+			 * Used to avoid problems when opening many photos
+			 */
+			if (request.msg === "openAllPhotos")
 			{
-				chrome.windows.create({url: request.imgs[0]}, function(win) {
-					for( var i = 1; i < request.imgs.length; i++ )
-					{
-						chrome.tabs.create({windowId: win.id, url: request.imgs[i]});
-					}
-				});
+
+				if( confirm("Are you sure you want to open " + request.imgs.length + " tab(s)? It may take a while to download everything.") )
+				{
+					chrome.windows.create({url: request.imgs[0]}, function(win) {
+						for( var i = 1; i < request.imgs.length; i++ )
+						{
+							chrome.tabs.create({
+								windowId: win.id,
+								url: request.imgs[i]
+							});
+						}
+					});
+				}
+
 			}
 
-		}
+		});
 
-});
+})();
--- popup_script.js
+++ popup_script.js
@@ -1,165 +1,175 @@
-const apiUrl = "http://api.flickr.com/services/rest/?method=";
-const apiKeyParam = "&api_key=07e391a86e66960a7449ea81d0d9cd48";
+(function(){
 
-const perPage = "&per_page=30";
-var page = 1;
+	"use strict";
 
-/**
- * Get all picture sizes and create thumbnail
- */
-function getPicture(id)
-{
-	$.ajax({
-		url: apiUrl + 'flickr.photos.getSizes&photo_id=' + id + apiKeyParam,
-		dataType: "xml",
+	var apiUrl = "http://api.flickr.com/services/rest/?method=";
+	var apiKeyParam = "&api_key=07e391a86e66960a7449ea81d0d9cd48";
+	var perPage = "&per_page=30";
+	var page = 1;
 
-		success: function(xml) {
+	/**
+	 * Get all picture sizes and create thumbnail
+	 */
+	function getPicture(id)
+	{
+		$.ajax({
+			url: apiUrl + "flickr.photos.getSizes&photo_id=" + id + apiKeyParam,
+			dataType: "xml",
 
-			var thumbUrl = $(xml).find('size').first().attr('source');
-			var bigUrl = $(xml).find('size').last().attr('source');
+			success: function(xml) {
 
-			if( thumbUrl && bigUrl )
-			{
+				var size = $(xml).find("size");
+				var thumbUrl = size.first().attr("source");
+				var bigUrl = size.last().attr("source");
 
-				var img = document.createElement("image");
-				img.src = thumbUrl;
+				if( thumbUrl && bigUrl )
+				{
 
-				var link = document.createElement("a");
-				link.href = bigUrl;
-				link.target = "_blank";
-				$(link).click(function() {
-					// Used to, maybe, replace the URL when clicking with the download URL
-					link.href = getPhotoURL( link.href );
+					var img = $("<img/>")
+						.attr("src", thumbUrl);
+
+					var link = $("<a/>")
+						.on("click", function() {
+
+							// Used to, maybe, replace the URL when clicking with the download URL
+							link.attr( "href", getPhotoURL( link.attr("href") ) );
+
+						})
+						.attr({
+							"href" : bigUrl,
+							"target" : "_blank"
+						})
+						.append( img )
+						.appendTo( $("#imgContainer") );
+
+					$("#imgContainer, #disclaimer").show();
+
+				}
+
+			}
+
+		});
+	}
+
+	/**
+	 * Get all pictures inside some Photoset
+	 */
+	function getPhotoset(id)
+	{
+		$.ajax({
+			url: apiUrl + "flickr.photosets.getPhotos&media=photos&photoset_id=" + id + perPage + "&page=" + page + apiKeyParam,
+			dataType: "xml",
+
+			success: function(xml) {
+
+				$(xml).find("photo").each(function(){
+					getPicture( $(this).attr("id") );
 				});
 
-				link.appendChild(img);
-				$("#imgContainer").append(link);
+				page++;
+				$("#setOk").html("+30");
 
-				$("#imgContainer").show();
-				$("#disclaimer").show();
+			}
 
+		});
+	}
+
+	/**
+	 * Get the URL to open or download the big photo
+	 */
+	function getPhotoURL(url)
+	{
+		if( $("#auto").attr("checked") )
+		{
+			var urlArr = url.split(".");
+			urlArr[urlArr.length - 2] += "_d";
+			return urlArr.join(".");
+		}
+
+		return url;
+	}
+
+	/**
+	 * Button handlers
+	 */
+	$(document).ready(function()
+	{
+		// Photo "Get" button
+		$("#photoOk").on("click", function()
+		{
+			if( $("#photoId").attr("value") )
+			{
+				getPicture( $("#photoId").attr("value") );
+			}
+		});
+
+		// Set "Get" button
+		$("#setOk").on("click", function()
+		{
+			if( $("#setId").attr("value") )
+			{
+				getPhotoset( $("#setId").attr("value") );
+			}
+		});
+
+		// Reset the page and the Set button
+		$("#setId").on("keydown", function() {
+			page = 1;
+			$("#setOk").html("Get");
+		});
+
+		// Open all photos
+		$("#openAll").on("click", function()
+		{
+			var imgs = [];
+
+			$("img").each(function() {
+				imgs.push( getPhotoURL( $(this).parent().attr("href") ) );
+			});
+
+			if( imgs.length > 0 )
+			{
+				chrome.extension.sendRequest({
+					msg: "openAllPhotos",
+					imgs: imgs
+				});
+			}
+		});
+
+		// Clear photos
+		$("#clear").on("click", function()
+		{
+			$("img").each(function() {
+				$(this).parent().remove();
+			});
+
+			page = 1;
+			$("#setOk").html("Get");
+		});
+	});
+
+	/**
+	 * Get the ID from the URL, when possible
+	 */
+	chrome.tabs.getSelected(null, function(tab) {
+
+		if( tab.url.indexOf("flickr.com\/photos") > -1 )
+		{
+			var urlParts = tab.url.split("/");
+
+			// Set
+			if( urlParts[5] === "sets")
+			{
+				$("#setId").attr("value", urlParts[6]);
+			}
+			// Photo
+			else if( urlParts.length >= 6 )
+			{
+				$("#photoId").attr("value", urlParts[5]);
 			}
 
 		}
 
 	});
-}
 
-/**
- * Get all pictures inside some Photoset
- */
-function getPhotoset(id)
-{
-	$.ajax({
-		url: apiUrl + 'flickr.photosets.getPhotos&media=photos&photoset_id=' + id + perPage + "&page=" + page + apiKeyParam,
-		dataType: "xml",
-
-		success: function(xml) {
-
-			$(xml).find('photo').each(function(){
-				getPicture( $(this).attr('id') );
-			});
-
-			page++;
-			$("#setOk").html("+30");
-
-		}
-
-	});
-}
-
-/**
- * Get the URL to open or download the big photo
- */
-function getPhotoURL(url)
-{
-	if( $("#auto").attr('checked') )
-	{
-		var urlArr = url.split(".");
-		urlArr[urlArr.length - 2] += "_d";
-		return urlArr.join(".");
-	}
-
-	return url;
-}
-
-/**
- * Button handlers
- */
-$(document).ready(function()
-{
-	// Photo "Get" button
-	$("#photoOk").click(function()
-	{
-		if( $("#photoId").attr('value') )
-		{
-			getPicture( $("#photoId").attr('value') );
-		}
-	});
-
-	// Set "Get" button
-	$("#setOk").click(function()
-	{
-		if( $("#setId").attr('value') )
-		{
-			getPhotoset( $("#setId").attr('value') );
-		}
-	});
-
-	// Reset the page and the Set button
-	$("#setId").keydown(function() {
-		page = 1;
-		$("#setOk").html("Get");
-	});
-
-	// Open all photos
-	$("#openAll").click(function()
-	{
-		var imgs = [];
-
-		$("img").each(function() {
-			imgs.push( getPhotoURL( $(this).parent().attr("href") ) );
-		});
-
-		if( imgs.length > 0 )
-		{
-			chrome.extension.sendRequest({msg: "openAllPhotos", imgs: imgs});
-		}
-	});
-
-	// Clear photos
-	$("#clear").click(function()
-	{
-		$("img").each(function() {
-			$(this).parent().remove();
-		});
-
-		page = 1;
-		$("#setOk").html("Get");
-	});
-});
-
-/**
- * Get the ID from the URL, when possible
- */
-chrome.tabs.getSelected(null, function(tab) {
-
-	if( tab.url.indexOf('flickr.com\/photos') > -1 )
-	{
-		var urlParts = tab.url.split("/");
-
-		// Set
-		if( urlParts[5] == "sets")
-		{
-			$("#setId").attr('value', urlParts[6]);
-		}
-		// Photo
-		else if( urlParts.length >= 6 )
-		{
-			$("#photoId").attr('value', urlParts[5]);
-		}
-
-	}
-
-
-});
+})();
